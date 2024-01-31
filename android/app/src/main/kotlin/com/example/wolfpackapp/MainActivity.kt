fadf71b36279f4453955ff6a43c56fde5a2c878f
ThrillerGriller <thrillergriller1234@gmail.com>
2024-01-30 19:03:44 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/wolfpackapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wolfpackapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/HomeScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends MaterialApp{
+
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
+}
