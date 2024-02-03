d3bfa91c99a8329740405cdb8e47e0efe8d83e54
kwiby <134721335+kwiby@users.noreply.github.com>
2024-02-03 15:12:31 -0500

    Added menu drawer button to settings page.

--- lib/screens/settings_screen/settings_screen.dart
+++ lib/screens/settings_screen/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../menu_drawer.dart';
+
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -7,10 +9,23 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      /*
+      ####################
+      #=-=-= AppBar =-=-=#
+      ####################
+      */
       appBar: AppBar(
           title: const Text('Settings'),
           centerTitle: true,
-          foregroundColor: Theme.of(context).colorScheme.onSurface),
+          foregroundColor: Theme.of(context).colorScheme.onSurface
+      ),
+
+      /*
+      #########################
+      #=-=-= Menu Drawer =-=-=#
+      #########################
+      */
+      drawer: const MenuDrawer(),
     );
   }
 }
