778672f533e7d68286df55e2415f95cb1afe2c7f
Edison Cai <thrillergriller1234@gmail.com>
2024-10-06 14:21:38 -0400

    Patch v0.6.2 - Internet Connect & Offline Dialog Files

---
+++ lib/screens/user_offline_dialog.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wolfpackapp/screens/login_screen/login_screen.dart';
+
+import '../models_services/teachassist_model.dart';
+import '../misc/page_navigator.dart';
+import '../misc/shared_prefs.dart';
+
+class UserOfflineDialog extends Container {
+  UserOfflineDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top:15, right: 15, left: 15),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Icon(
+            Icons.language,
+            size: 32,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FittedBox(
+                fit: BoxFit.contain,
+                child: Text('Unable To Connect',
+                    style: GoogleFonts.roboto(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900)),
+              ),
+          
+              FittedBox(
+              fit: BoxFit.contain,
+              child: Text('Reconnect For More Features',
+                  style: GoogleFonts.roboto(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
